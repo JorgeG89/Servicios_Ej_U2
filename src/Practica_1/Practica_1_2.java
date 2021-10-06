153db7b259fb09bd859e27a6b9020f40522e4ea1
Jorge Mañas Garcia <alu2161@ieselcaminas.org>
2021-10-06 15:09:55 +0200

    Servicios_EJ_U2_JorgeMG

--- src/Practica_1/Practica_1_2.java
+++ src/Practica_1/Practica_1_2.java
@@ -3,26 +3,23 @@ package Practica_1;
 public class Practica_1_2 {
     public static void main(String[] args) {
 
-        Thread t1 = new Thread(new Tarea1_2());
-        Thread t2 = new Thread(new Tarea2_2());
+        Thread t1 = new Thread(new Tarea1_2(0));
+        Thread t2 = new Thread(new Tarea1_2(1));
         t1.start();
         t2.start();
     }
 }
 
 class Tarea1_2 implements Runnable {
+    int idHebra;
+    public Tarea1_2(int idHebra) {
+        this.idHebra = idHebra;
+    }
     public void run() {
         int numHebras = 1000;
         for (int nHebra = 0; nHebra < numHebras; nHebra++) {
-            System.out.println("Hola, soy la hebra 0");
+            System.out.println("Hola, soy la hebra "+idHebra);
         }
     }
 }
-class Tarea2_2 implements Runnable {
-    public void run() {
-        int numHebras = 1000;
-        for (int nHebra = 0; nHebra < numHebras; nHebra++) {
-            System.out.println("Hola, soy la hebra 1");
-        }
-    }
-}
+
